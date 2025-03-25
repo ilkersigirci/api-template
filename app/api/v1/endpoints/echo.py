1977b83b72b1647b7a0e690052ae83ef685a32b1
ilkersigirci <sigirci.ilker@gmail.com>
2025-03-25 11:22:52 +0300

    echo logs added

--- app/api/v1/endpoints/echo.py
+++ app/api/v1/endpoints/echo.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from loguru import logger
 
 from app.models.echo import Message
 
@@ -15,4 +16,7 @@ async def send_echo_message(incoming_message: Message) -> Message:
     Returns:
         message same as the incoming.
     """
+    logger.error(f"Echo dummy log error: {incoming_message}")
+    print(f"Echo dummy print: {incoming_message}")  # noqa: T201
+
     return incoming_message
