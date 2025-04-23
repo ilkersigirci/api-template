f934665256c19b866b04a3ce00610cc1f78734cc
ilkersigirci <sigirci.ilker@gmail.com>
2025-04-23 23:52:09 +0300

    conftest updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,20 @@ from app.api.application import get_app
 from app.api.auth.utils import create_access_token
 from app.api.redis.deps import get_redis_pool
 from app.api.users.schemas import User
+from app.core.settings import settings
+from app.db.deps import get_db_session
+from app.db.utils import create_database, drop_database
 from fakeredis import FakeServer
 from fakeredis.aioredis import FakeConnection
 from fastapi import FastAPI
 from httpx import ASGITransport, AsyncClient
 from redis.asyncio import ConnectionPool
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 
 @pytest.fixture(scope="session")
@@ -20,6 +29,63 @@ def anyio_backend() -> str:
         Backend name
     """
     return "asyncio"
+
+
+@pytest.fixture(scope="session")
+async def _engine() -> AsyncGenerator[AsyncEngine, None]:
+    """Create engine and databases.
+
+    Yields:
+        New db engine.
+    """
+    from app.db.meta import meta
+    from app.db.utils import load_all_db_models
+
+    load_all_db_models()
+
+    await create_database()
+
+    engine = create_async_engine(str(settings.DB_URL))
+    async with engine.begin() as conn:
+        await conn.run_sync(meta.create_all)
+
+    try:
+        yield engine
+    finally:
+        await engine.dispose()
+        await drop_database()
+
+
+@pytest.fixture
+async def dbsession(
+    _engine: AsyncEngine,
+) -> AsyncGenerator[AsyncSession, None]:
+    """Get session to database.
+
+    Fixture that returns a SQLAlchemy session with a SAVEPOINT, and the rollback to it
+    after the test completes.
+
+    Args:
+        _engine: current engine.
+
+    Yields:
+        session: async session.
+    """
+    connection = await _engine.connect()
+    trans = await connection.begin()
+
+    session_maker = async_sessionmaker(
+        connection,
+        expire_on_commit=False,
+    )
+    session = session_maker()
+
+    try:
+        yield session
+    finally:
+        await session.close()
+        await trans.rollback()
+        await connection.close()
 
 
 @pytest.fixture
@@ -40,6 +106,7 @@ async def fake_redis_pool() -> AsyncGenerator[ConnectionPool, None]:
 
 @pytest.fixture
 def fastapi_app(
+    dbsession: AsyncSession,
     fake_redis_pool: ConnectionPool,
 ) -> FastAPI:
     """Fixture for creating FastAPI app.
@@ -48,6 +115,7 @@ def fastapi_app(
         Fastapi app with mocked dependencies.
     """
     application = get_app()
+    application.dependency_overrides[get_db_session] = lambda: dbsession
     application.dependency_overrides[get_redis_pool] = lambda: fake_redis_pool
     return application
 
